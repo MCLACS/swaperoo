e8073404c85b0c9aef6d1f6499d2ef3c89940f87
Ben Michener <bm5616@mcla.edu>
2019-03-07 16:25:08 +0000

    Whoo screw the rules

--- MyCoolProgram.java
+++ MyCoolProgram.java
@@ -2,9 +2,6 @@ public class MyCoolProgram
 {
     public static void main(String args[])
     {
-        System.out.println("Hello Dude!");
-        System.out.println("Whoa cool!");
-        System.out.println("Hello World: Python");
-        System.out.println("Something sarcastic.");
+        System.out.println("Ben Rules!");
     }
 }
