7e77734f577d74ed519cccba2c178b8f6d02b6cd
Darrien Smith <ds7526@mcla.edu>
2019-03-07 16:23:15 +0000

    inclass

--- MyCoolProgram.java
+++ MyCoolProgram.java
@@ -6,5 +6,6 @@ public class MyCoolProgram
         System.out.println("Whoa cool!");
         System.out.println("Hello World: Python");
         System.out.println("Something sarcastic.");
+        System.out.println("According to all known laws of aviation,there is no way a bee should be able to fly.");
     }
 }
