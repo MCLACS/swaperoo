428c8e5079233fe29bd28be5ee1d264ad8d73dfd
HermitXVII <a30103276@mcla.edu>
2019-03-07 16:30:48 +0000

    Merged

--- MyCoolProgram.java
+++ MyCoolProgram.java
@@ -4,19 +4,18 @@ public class MyCoolProgram
     {
         System.out.println("Hello Dude!");
         System.out.println("Whoa cool!");
-<<<<<<< HEAD
         System.out.println("Hello World: Python")
-        
-        while(true)
-        {
-            
-            main();
-        }
-=======
-        System.out.println("Hello World: Python");
         System.out.println("Something sarcastic.");
         System.out.println("According to all known laws of aviation,there is no way a bee should be able to fly.");
         System.out.println("Meme *clap* review!");
->>>>>>> 61fae0c605fe92b2cc237c9cce481b031c187c56
+        
+        while(true)
+        {
+            main();
+        }
+
+      
     }
+
+    
 }
