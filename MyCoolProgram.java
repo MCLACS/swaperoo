378414f40180078a640ed9f97a0260ba5d4e1908
Andrew Allen <aa2630@mcla.edu>
2019-03-07 16:18:45 +0000

    This is an exercise.

--- MyCoolProgram.java
+++ MyCoolProgram.java
@@ -5,5 +5,6 @@ public class MyCoolProgram
         System.out.println("Hello Dude!");
         System.out.println("Whoa cool!");
         System.out.println("Hello World: Python")
+        System.out.println("Something sarcastic.");
     }
 }
