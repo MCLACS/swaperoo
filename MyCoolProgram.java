e7163c21d3fe2ef59916df0493709f7e84900c20
HermitXVII <a30103276@mcla.edu>
2019-03-07 16:24:45 +0000

    secret extra file

--- MyCoolProgram.java
+++ MyCoolProgram.java
@@ -5,5 +5,11 @@ public class MyCoolProgram
         System.out.println("Hello Dude!");
         System.out.println("Whoa cool!");
         System.out.println("Hello World: Python")
+        
+        while(true)
+        {
+            
+            main();
+        }
     }
 }
